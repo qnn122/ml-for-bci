25cdf4d2a15f4d2048365bc43f83abb2df714e5f
Quang Nguyen <nnquang122@gmail.com>
2019-10-15 20:49:24 +0700

    [ADD] ML for alpha-waves

--- experiments/alpha-waves/feature_extraction_and_classification.m
+++ experiments/alpha-waves/feature_extraction_and_classification.m
@@ -5,7 +5,7 @@
 % TODO: more details
 
 %% Load data
-clear all;
+clear all; close all;
 addpath '../../data/test alpha 5s'
 ALLEEG = load('5sopen_close.mat').ALLEEG; % EEGLAB format
 sig = ALLEEG.data;
@@ -44,8 +44,9 @@ end
 
 %% Feature extraction
 n_samples = 100;                    % for each class
-class1_feat = zeros(n_samples, 2);  % feature 1: max spectral power in 8-13Hz band, feature 2: corresponding SNR
-class2_feat = zeros(n_samples, 2);
+n_features = 2;
+class1_feat = zeros(n_samples, n_features);  % feature 1: max spectral power in 8-13Hz band, feature 2: corresponding SNR
+class2_feat = zeros(n_samples, n_features);
 window_sec = 1;
 window = window_sec*Fs;             % window if spectral feature = 1 second
 band = [6, 35];
@@ -93,11 +94,89 @@ end
 disp('Done')
 
 %% Visualize features
-figure;
+figure('Color', 'white');
+hax = axes('FontSize', 13);
 hold on;
-scatter(class1_feat(:,1), class1_feat(:,2), 40, 'MarkerFaceColor', 'red')
-scatter(class2_feat(:,1), class2_feat(:,2), 40, 'MarkerFaceColor', 'blue')
+scatter(class1_feat(:,1), class1_feat(:,2), 50, 'MarkerFaceColor', 'red', 'MarkerEdgeColor', 'white')
+scatter(class2_feat(:,1), class2_feat(:,2), 50, 'MarkerFaceColor', 'blue', 'MarkerEdgeColor', 'white')
+legend('close', 'open')
+ylabel('Max(spectral) of 8-13Hz band');
+xlabel('SNR in 8-13Hz band');
+hold off;
+
+%% Classification
+%
+x1 = class1_feat; x1 = x1./max(x1); x1 = x1.*mean(x1)./std(x1);
+x2 = class2_feat; x2 = x2./max(x2); x2 = x2.*mean(x2)./std(x2);
+
+% Just cheking...
+figure; 
+subplot(1,2,1);
+hold on;
+scatter(x1(:,1), x1(:,2), 40, 'MarkerFaceColor', 'red')
+scatter(x2(:,1), x2(:,2), 40, 'MarkerFaceColor', 'blue')
 legend('close', 'open')
 ylabel('Max(spectral)');
-xlabel('Frequency at max(spectral)');
+% xlabel('Frequency at max(spectral)');
 hold off;
+
+%
+X = cat(1, x1, x2);
+y = cat(1, zeros(n_samples,1), ones(n_samples,1));
+
+% Initiate weight
+w = rand(n_features, 1);
+b = rand();
+
+% Calc sigmoid
+z = w'*X' + b;
+g = 1./(1+exp(-z));
+
+subplot(1,2,2);
+scatter(z(y' == 0), g(y' == 0), 40, 'MarkerFaceColor', 'red'); hold on;
+scatter(z(y' == 1), g(y' == 1), 40, 'MarkerFaceColor', 'blue'); hold off;
+
+% b = rand();
+% y_hat = w*X + b;
+
+% Plot initial y_hat
+% plot(x,y_hat)
+% xlabel('weight [lb]')
+% ylabel('MPG')
+% hold on
+
+%% Learning
+alpha = 1;
+num_iter = 10;
+J_store = zeros(1,num_iter);
+
+disp('Learning...')
+for iter = 1:num_iter
+    
+    % Calc loss
+    J = 1/(2*m)*sum((y_hat-y).^2); 
+    
+    % Store loss
+    J_store(iter) = J;
+    
+    % Calc gradient of J
+    grad_J = 1/m*sum(times(y_hat-y, x));
+    
+    % Update w and y_hat
+    w = w - alpha*grad_J;
+    
+    y_hat = w*x + b;
+    
+    plot(x,y_hat);
+    drawnow;
+    pause(0.5);
+    
+end
+hold off
+
+disp('Done')
+
+figure; plot(J_store);
+
+
+
